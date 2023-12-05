ef40615599277b19348040ff82b8f5b57e1e3d0d
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2023-12-05 20:28:58 +0900

    [ADD] 서버 구현
    메인_서버(임시) : 완성되지않은 테스트용 서버
    블루투스_서버 : UART 를 이용한 블루투스통신서버
    TCP_서버 : 라즈베리파이간의 통신서버

---
+++ DEMO/TCP_서버.c
@@ -0,0 +1,97 @@
+/*
+컴파일시
+gcc -o TCP_서버 TCP_서버.c -lrt -lwiringPi
+실행시
+./TCP_서버
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <mqueue.h>
+
+#define PORT 8080
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    mqd_t mq;
+    const char* mq_name = "/posix_mq";
+
+    // 메시지 큐 오픈
+    mq = mq_open(mq_name, O_WRONLY);
+    if(mq == (mqd_t)-1) {
+        perror("mq_open");
+        exit(EXIT_FAILURE);
+    }
+
+    // 소켓 생성
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // 소켓 옵션 설정
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // 소켓에 주소 바인딩
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // 서버 소켓 대기
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    while(1) {
+        printf("Waiting for new connection...\n");
+
+        // 클라이언트 연결 수락
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
+            perror("accept");
+            continue;
+        }
+
+        printf("Connection established\n");
+
+        // 클라이언트와 통신
+        while(1) {
+            memset(buffer, 0, 1024);
+            int valread = read(new_socket, buffer, 1024);
+            if(valread == 0) {
+                printf("Client disconnected\n");
+                break;
+            }
+            printf("Received: %s\n", buffer);
+            
+            // 메시지 큐로 데이터 전송
+            if(mq_send(mq, buffer, strlen(buffer), 0) == -1) {
+                perror("mq_send");
+                break;
+            }
+        }
+
+        close(new_socket);
+    }
+
+    // 메시지 큐 닫기
+    mq_close(mq);
+
+    return 0;
+}
---
+++ DEMO/블루투스_서버.c
@@ -0,0 +1,119 @@
+/*
+컴파일시
+gcc -o 블루투스_서버 블루투스_서버.c -lrt -lwiringPi
+실행시
+./블루투스_서버
+*/
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <wiringPi.h>
+#include <wiringSerial.h>
+#include <mqueue.h>
+#include <stdbool.h>
+
+#define BAUD_RATE 115200
+static const char *UART2_DEV = "/dev/ttyAMA1";
+static const char *MQ_NAME = "/posix_mq";
+#define MAX_MSG_SIZE 256
+#define AUTO 17
+
+unsigned char serialRead(const int fd);
+void serialWrite(const int fd, const unsigned char c);
+
+unsigned char serialRead(const int fd) {
+    unsigned char x;
+    if (read(fd, &x, 1) != 1)
+        return -1;
+    return x;
+}
+
+void serialWrite(const int fd, const unsigned char c) {
+    write(fd, &c, 1);
+}
+
+void serialWriteString(const int fd, const char *str) {
+    while (*str) {
+        write(fd, str, 1);
+        str++;
+    }
+}
+
+bool isExistMode(const int fd_serial, const char *isExistmode) {
+    char mode = isExistmode[0];
+    char value = isExistmode[1] - '0';
+    char output[50];
+
+    switch (mode) {
+        case 'T':
+            if(value == AUTO)
+                sprintf(output, "타이머모드 : 자동\n");
+            else
+                sprintf(output, "타이머모드 : %d분\n", value * 10);
+            serialWriteString(fd_serial, output);
+            return true;
+        case 'R':
+            if(value == AUTO)
+                sprintf(output, "회전모드 : 자동\n");
+            else
+                sprintf(output, "회전모드 : %d단\n", value);
+            serialWriteString(fd_serial, output);
+            return true;
+        case 'P':
+            if(value == AUTO)
+                sprintf(output, "속도모드 : 자동\n");
+            else
+                sprintf(output, "속도모드 설정 : %d단\n", value);
+            serialWriteString(fd_serial, output);
+            return true;
+        default:
+            return false;
+    }
+}
+
+int main() {
+    int fd_serial;
+    unsigned char dat, dat2;
+    mqd_t mq;
+    struct mq_attr attr;
+    char message[MAX_MSG_SIZE];
+
+    attr.mq_flags = 0;
+    attr.mq_maxmsg = 10;
+    attr.mq_msgsize = MAX_MSG_SIZE;
+    attr.mq_curmsgs = 0;
+
+    mq = mq_open(MQ_NAME, O_CREAT | O_WRONLY, 0644, &attr);
+    if (mq == (mqd_t)-1) {
+        perror("mq_open");
+        return 1;
+    }
+
+    if (wiringPiSetup() < 0)
+        return 1;
+
+    if ((fd_serial = serialOpen(UART2_DEV, BAUD_RATE)) < 0) {
+        printf("Unable to open serial device.\n");
+        return 1;
+    }
+
+    while (1) {
+        if (serialDataAvail(fd_serial)) {
+            dat = serialRead(fd_serial);
+            if (serialDataAvail(fd_serial)) {
+                dat2 = serialRead(fd_serial);
+                sprintf(message, "%c%c", dat, dat2);
+
+                if (isExistMode(fd_serial, message)) {
+                    if (mq_send(mq, message, 2, 0) == -1) {
+                        perror("mq_send");
+                    }
+                }
+            }
+        }
+        delay(10);
+    }
+
+    mq_close(mq);
+    return 0;
+}
