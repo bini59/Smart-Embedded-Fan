24554bcec866f8c496d8ee92ad42182b88cda0e9
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2023-12-08 21:21:07 +0900

    [BUG] 실패한 서버 12.08
    12.08
    DHT 추가했는데 쓰레드가 서로 간섭해서 꺼지는 문제점 발생

--- SeungJae/12.7일까지한거/Server.c
+++ SeungJae/12.7일까지한거/Server.c
@@ -1,10 +1,9 @@
 /*
 컴파일시
-gcc -o Server Server.c -lrt -lwiringPi
+gcc -o Server Server.c -lrt -lwiringPi -lpthread
 실행시
 sudo ./Server
 */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -14,38 +13,45 @@ sudo ./Server
 #include <wiringPi.h>
 #include <pthread.h>
 #include <sys/time.h>
-
+#include <stdlib.h>
 #include <stdint.h>
+
 
 #define IN_A 18
 #define IN_B 19
 #define AUTO 17
 #define TRIG 24
 #define ECHO 23
-#define MAX_TIMINGS 85
 #define DHT_PIN 26
 
-#define DELAY_MS 1000  // 데이터 읽기 사이의 지연 시간 
+#define MAX_TIMINGS 85
+#define DELAY_MS 10000  // 데이터 읽기 사이의 지연 시간 
+#define TIMEOUT 10000 // 타임아웃을 위한 카운터 한계
+
 int data[5] = {0, 0, 0, 0, 0};
 int pin_arr[4] = {12, 16, 20, 21};
 int one_two_phase[8][4] = {{1,0,0,0}, {1,1,0,0}, {0,1,0,0}, {0,1,1,0}, {0,0,1,0}, {0,0,1,1}, {0,0,0,1},{1,0,0,1}};
 const char* name = "/posix_mq";
-char mode;
-int amount;
+char mode; // Mode 값
+int amount; // 1단 2단 결정하는 값
 
-pthread_t rotate_thread;
-pthread_t tid_dht;
-pthread_mutex_t lock;
-pthread_mutex_t lock_d;
-bool rotate_thread_running = false;
-float sharedDistance = 0.0;
+pthread_t dht_thread; // 온습도센서 쓰레드
+pthread_t rotate_thread; // 회전모터 쓰레드
+pthread_t fan_thread; // 선풍기 모터 쓰레드
 
+pthread_mutex_t lock_dht; // 온습도 센서용 Lock
+pthread_mutex_t lock_d; // 초음파 센서용 Lock
+pthread_mutex_t lock; // 회전 센서용 Lock
 
-void *read_dht_data() {
+bool rotate_thread_running = false; // 회전 모터 On/Off 용 변수
+float sharedDistance = 0.0; // 전역 변수로 거리 저장
+float humidity, temperature; // 전역 변수로 습도와 온도를 저장
+
+void read_dht_data() {
     uint8_t laststate = HIGH;
     uint8_t counter = 0;
     uint8_t j = 0, i;
-    int attempts = 123453145; // 재시도 횟수
+    int attempts = 5; // 재시도 횟수
 
     while (attempts) {
         data[0] = data[1] = data[2] = data[3] = data[4] = 0;
@@ -55,18 +61,22 @@ void *read_dht_data() {
         delay(18);
         pinMode(DHT_PIN, INPUT);
 
+        uint32_t timeout_counter = 0; // 타임아웃 카운터 초기화
+
         for (i = 0; i < MAX_TIMINGS; i++) {
             counter = 0;
             while (digitalRead(DHT_PIN) == laststate) {
                 counter++;
                 delayMicroseconds(1);
-                if (counter == 255) {
+                if (counter == 255 || ++timeout_counter > TIMEOUT) {
                     break;
                 }
             }
             laststate = digitalRead(DHT_PIN);
 
-            if (counter == 255) break;
+            if (counter == 255 || timeout_counter > TIMEOUT) {
+                break;
+            }
 
             if ((i >= 4) && (i % 2 == 0)) {
                 data[j / 8] <<= 1;
@@ -79,14 +89,25 @@ void *read_dht_data() {
         // 데이터 유효성 체크
         if ((j >= 40) && (data[4] == ((data[0] + data[1] + data[2] + data[3]) & 0xFF)) &&
             (data[0] != 0 || data[2] != 0)) {
-            printf("Humidity = %d.%d %% Temperature = %d.%d C\n",
-                data[0], data[1], data[2], data[3]);
+            pthread_mutex_lock(&lock_dht);
+            float tmp1 = (float)data[0] + data[1] / 10.0;
+            float tmp2 = (float)data[2] + data[3] / 10.0;
+            if(tmp1 < 100 || tmp1 > 0)
+                humidity = tmp1;
+            if(tmp2 < 50 || tmp2 > -20)
+                temperature = tmp2;
+            pthread_mutex_unlock(&lock_dht);
+            printf("Humidity = %.1f %% Temperature = %.1f C\n", humidity, temperature);
+            return; // 성공적으로 데이터를 읽으면 함수 종료
         } else {
-            printf("Data not good, skip\n");
             attempts--;
-            
+            delay(DELAY_MS); // 재시도 전에 지연 시간 추가
         }
-        delay(DELAY_MS); // 재시도 전에 지연 시간 추가
+    }
+}
+void* readTemperatrue_Humidity(){
+    while (1) {
+        read_dht_data();
     }
 }
 
@@ -100,15 +121,12 @@ void setMotor(int speed, int direction) {
     }
     pwmWrite(IN_A, speed);
 }
-
-
 // 현재 시간을 마이크로초로 반환하는 함수
 long getMicrotime(){
     struct timeval currentTime;
     gettimeofday(&currentTime, NULL);
     return currentTime.tv_sec * (int)1e6 + currentTime.tv_usec;
 }
-
 // 거리를 측정하는 함수
 float measureDistance() {
     long start, stop;
@@ -177,13 +195,13 @@ void *distanceThread(void *arg) {
         pthread_mutex_lock(&lock_d);
         sharedDistance = measureDistance();
         pthread_mutex_unlock(&lock_d);
-        printf("thred is on!!\n");
+        printf("distance thread is on!!\n");
 
         if(amount == AUTO && mode == 'P'){
             printf("Amount is Auto!!\n");
             if(sharedDistance > 1000) setMotor(300,1);
             else setMotor(sharedDistance*0.25, 1);
-        }else{
+        }else if(mode == 'P'){
             setMotor(100*(amount), 1);                    
         }
         delay(200); // 5Hz 주파수 유지
@@ -216,7 +234,7 @@ void *rotate(void *arg) {
     pthread_mutex_lock(&lock);
     rotate_thread_running = true;
     pthread_mutex_unlock(&lock);
-    printf("Roate Auto mode is on!!\n");
+    printf("Roate mode is on!!\n");
     while (rotate_thread_running) {
         one_two_Phase_Rotate_angle(130, 1); // 130도 회전
         one_two_Phase_Rotate_angle(130, 0); // 0도로 회전
@@ -237,23 +255,22 @@ int main(int argc, char **argv) {
     attr.mq_curmsgs = 0;
 
     init();
-    pthread_t tid;
     pthread_mutex_init(&lock, NULL);
     pthread_mutex_init(&lock_d, NULL);
-    pthread_create(&tid, NULL, &distanceThread, NULL);
-    pthread_create(&tid_dht, NULL, &read_dht_data, NULL);
+    pthread_create(&fan_thread, NULL, &distanceThread, NULL);
+    pthread_create(&dht_thread, NULL, &readTemperatrue_Humidity, NULL);
     
     mq = mq_open(name, O_CREAT | O_RDONLY, 0644, &attr);
     while(1) {
         n = mq_receive(mq, buf, sizeof(buf), NULL);
         buf[n] = '\0';
+        if(isOutOfRange( buf[0], buf[1] - '0')) {
+            continue; // 범위를 벗어난 경우 반복 건너뛰기
+        }
         mode = buf[0];
         amount = buf[1] - '0';
         printf("%c %d\n",mode,amount);
-        if(isOutOfRange(mode, amount)) {
-            continue; // 범위를 벗어난 경우 반복 건너뛰기
-        }
-
+        
         if (mode == 'R' && amount == 1) {
             if (!rotate_thread_running) {
                 pthread_create(&rotate_thread, NULL, rotate, NULL);
@@ -269,8 +286,9 @@ int main(int argc, char **argv) {
 
     mq_close(mq);
     mq_unlink(name);
-    pthread_join(tid, NULL);
-    pthread_join(tid_dht, NULL);
+    pthread_join(fan_thread, NULL);
+    pthread_join(dht_thread,NULL);
     pthread_mutex_destroy(&lock);
+    pthread_mutex_destroy(&lock_d);
     return 0;
 }
